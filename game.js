f7795e79db04823dde27fe202ae19c8aba98f981
Lin Chen <lchen00000@gmail.com>
2014-11-12 16:03:22 -0800

    buy, sell stats created and works perfectly :D can alternate numbers if wanted

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ var mainMenu = {
         this.clouds.scale.x = 0.1; this.clouds.scale.y = 0.1;
         this.clouds.enableBody = true;
         this.clouds.createMultiple(30, 'cloud');
-        game.time.events.loop(2000, this.addCloud, this);
+        game.time.events.loop(1000, this.addCloud, this);
         
         //start button
         this.start = game.add.button(0,0, 'start', this.start);
@@ -39,6 +39,7 @@ var mainMenu = {
     },
     
     addCloud: function(){
+        //animating clouds
         var cloud = this.clouds.getFirstDead();
         cloud.reset(game.stage.width * 9 , Math.random() * 1000);
         cloud.body.velocity.x = Math.floor(Math.random() * -500 - 300);
@@ -52,32 +53,67 @@ var mainMenu = {
 };
 
 var mainState = {
-    // Here we add all the functions we need for our state
-    // For this project we will just have 3 functions
     preload: function () {
-        // This function will be executed at the beginning
-        // That's where we load the game's assets
-        game.load.image('logo', 'imgs/missionbit.png');
+        game.load.image('sky', 'imgs/skyBackground.png');
+        game.load.image('fence', 'imgs/horizontalFence.svg');
+        game.load.image('llama' , 'imgs/placeholder.png');
+        game.load.image('bar', 'imgs/bar.png');
+        game.load.image('buy', 'imgs/buyButton.png');
+        game.load.image('sell', 'imgs/sellButton.png');
     },
     create: function () {
-        // This function is called after the preload function
-        // Here we set up the game, display sprites, etc.
-
-        // Create a game sprite from the logo image positioned
-        // at the center of the game world
-        this.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        // The position of the sprite should be based on the
-        // center of the image (default is top-left)
-        this.sprite.anchor.setTo(0.5, 0.5);
-        // Change background color to a gray color
-        game.stage.backgroundColor = '#999999';
-    },
-    update: function () {
-        // This function is called 60 times per second
-        // It contains the game's logic
+        //backgrounds
+        this.bgSky = game.add.sprite(0,0, 'sky');
         
-        // Rotate the sprite by 1 degrees
-        this.sprite.angle += 1;
+        //fence
+        this.hFence = game.add.sprite(0,200, 'fence');
+        this.hFence.scale.x = 0.3; this.hFence.scale.y = 0.1;
+        this.vFence = game.add.sprite(this.hFence.width + this.hFence.height, 200, 'fence');
+        this.vFence.scale.x = 0.3; this.vFence.scale.y = 0.1;
+        this.vFence.angle = 90;
+        
+        //llama stuff
+        this.llamas = game.add.group(game, 'llamas','llama');
+        this.llamas.enableBody = true;
+        this.llamas.createMultiple(this.llamaCount, 'llamas');
+        this.llama = game.add.button(0, this.hFence.position.y -10, 'llama', this.llamaOnClick,             this);
+        this.llamaCount = 0;
+
+        //currency
+        this.money = 0;
+        
+        //text
+        this.llamaText = game.add.text(0,0,'Llamas: '+ this.llamaCount, { fontSize: '22px', fill: '#fff'});
+        this.moneyText = game.add.text(game.world.width/2 , 0, 'Money: $' + this.money,  { fontSize: '22px', fill: '#fff'});
+                
+    },
+    
+    update: function () {
+        //update texts
+        this.llamaText.setText('Llamas: '+ this.llamaCount);
+        this.moneyText.setText('Money: $' + this.money);
+    }, 
+    
+    llamaOnClick: function() {
+        this.bar = game.add.sprite(this.llama.position.x, this.llama.position.y + 20, 'bar');
+        this.bar.scale.x = 0.3; this.bar.scale.y = 0.1;
+        //buy button
+        this.buyButton = game.add.button(this.bar.position.x + 10, this.bar.position.y +0.1, 'buy',         this.buyOnClick, this);
+        this.buyButton.scale.x = 0.17; this.buyButton.scale.y = 0.17;
+        //sell button
+        this.sellButton = game.add.button(this.bar.position.x + this.bar.width /2, this.bar.position.y +0.1,               'sell', this.sellOnClick, this);
+        this.sellButton.scale.x = 0.17; this.sellButton.scale.y = 0.17;
+        
+    },
+    
+    buyOnClick: function () {
+        this.llamaCount ++;
+        this.money -= 100;
+    },
+    
+    sellOnClick: function() {
+        this.llamaCount --;
+        this.money += 50;
     }
 };
 
@@ -87,4 +123,4 @@ game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
 // And finally we tell Phaser to add and start our 'main' state
 game.state.add('main', mainState);
 game.state.add('mainMenu', mainMenu);
-game.state.start('mainMenu');
+game.state.start('main');
